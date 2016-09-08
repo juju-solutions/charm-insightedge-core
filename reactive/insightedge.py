6fa6154819447561e1f285028f44961708340db4
Cory Johns <johnsca@gmail.com>
2016-09-08 13:40:41 -0700

    Fix typos

--- reactive/insightedge.py
+++ reactive/insightedge.py
@@ -1,6 +1,6 @@
 # pylint: disable=unused-argument
 import subprocess
-from charms.reactive import when, when_not, when_none, is_state, set_state, remove_state
+from charms.reactive import when, when_not, is_state, set_state, remove_state
 from charmhelpers.core.hookenv import status_set, resource_get, log
 from charmhelpers.core.host import chdir
 from charmhelpers.core import unitdata
@@ -8,11 +8,11 @@ from distutils import dir_util
 
 
 @when_not('insightedge.fetched')
-def setup_insightedge_on_spark():
+def fetch_insightedge():
     result = resource_get('InsightEdgeDiff')
     if not result:
-        state_set("blocked", "Failed to fetch InsightEdge resource")
-        log.("Failed to fetch InsightEdge resource")
+        set_state("blocked", "Failed to fetch InsightEdge resource")
+        log("Failed to fetch InsightEdge resource")
         return
 
     unitdata.kv().set("insigthedgetarball", result)
@@ -29,7 +29,8 @@ def setup_insightedge_on_spark():
 @when('spark.ready')
 def setup_insightedge_on_spark(spark):
     status_set('waiting', 'Deploying InsightEdge')
-    files = dir_util.copy_tree('/var/tmp/InsightEdge-1.0.0-juju-diff', '/usr/lib/spark/')
+    files = dir_util.copy_tree('/var/tmp/InsightEdge-1.0.0-juju-diff',
+                               '/usr/lib/spark/')
     log("Files copied over from InsightEdge")
     for f in files:
         log(f)
@@ -48,7 +49,7 @@ def remove_insightedge_from_spark(spark):
 @when_not('insightedge.on.zeppelin')
 @when('insightedge.fetched')
 @when('zeppelin.ready')
-def setup_insightedge_on_spark(zeppelin):
+def setup_insightedge_on_zeppelin(zeppelin):
     update_status()
     set_state('insightedge.on.zeppelin')
 
